133e89b13a5a037075060ab6aefa991ab41b6985
Matt Stevens <matt@alloysoft.com>
2014-06-07 19:34:44 -0600

    Support additional lookups, use Launch Services version numbers
    Add support for querying the default application, as well as querying
    handlers of a given URL.
    
    Replace use of the CFBundleVersion as written with the Launch Services
    intepretation of the version number. This requires use of a private API,
    but provides a more accurate version comparison for the application's
    intended purpose - identifying duplicate bundles with the same version.
    Launch Services parses the string and creates its own representation of
    the version number that may discard additional information in the
    string, so a simple string comparison is inappropriate.

--- lsquery/main.m
+++ lsquery/main.m
@@ -1,7 +1,9 @@
 #import <Foundation/Foundation.h>
-#import "NSBundleAdditions.h"
+#import <getopt.h>
 
 extern OSStatus _LSCopyAllApplicationURLs(CFArrayRef *urls) __attribute__((weak_import));
+
+extern uint64_t _LSGetVersionFromString(CFStringRef versionString);
 
 /**
  * Returns an array of NSBundles for every application in the Launch Services database with the
@@ -35,73 +37,239 @@ static NSArray *BundlesForBundleIdentifier(NSString *bundleIdentifier) {
     return [bundles allObjects];
 }
 
-static void print_usage(void) {
-    printf("Usage: lsquery <bundle identifier>\n");
+/**
+ * Returns the default application for the given bundle identifier.
+ */
+static NSBundle *BundleForBundleIdentifier(NSString *bundleIdentifier) {
+    NSBundle *bundle = nil;
+    CFURLRef urlRef = NULL;
+    LSFindApplicationForInfo(0, (__bridge CFStringRef)bundleIdentifier, NULL, NULL, &urlRef);
+    if (urlRef != NULL) {
+        NSURL *bundleURL = CFBridgingRelease(urlRef);
+        bundle = [NSBundle bundleWithURL:bundleURL];
+    }
+    return bundle;
 }
 
-int main(int argc, const char * argv[]) {
+/**
+ * Returns all applications capable of handling the specified URL.
+ */
+static NSArray *BundlesForURL(NSURL *url) {
+    NSMutableSet *bundles = [NSMutableSet set];
+    NSArray *urls = CFBridgingRelease(LSCopyApplicationURLsForURL((__bridge CFURLRef)url, kLSRolesAll));
+    if (urls == nil) {
+        return nil;
+    }
+
+    for (NSURL *url in urls) {
+        NSBundle *bundle = [NSBundle bundleWithURL:url];
+        if (bundle != nil) {
+            [bundles addObject:bundle];
+        }
+    }
+
+    return [bundles allObjects];
+}
+
+/**
+ * Returns the default application for handling the specified URL.
+ */
+static NSBundle *BundleForURL(NSURL *url) {
+    NSBundle *bundle = nil;
+    CFURLRef urlRef = NULL;
+    LSGetApplicationForURL((__bridge CFURLRef)url, kLSRolesAll, NULL, &urlRef);
+    if (urlRef != NULL) {
+        bundle = [NSBundle bundleWithURL:(__bridge NSURL *)urlRef];
+        CFRelease(urlRef);
+    }
+    return bundle;
+}
+
+/**
+ * Returns the Launch Services version number for the specified bundle.
+ *
+ * This uses the private _LSGetVersionFromString() function to obtain Launch Services's
+ * intepretation of the bundle's version. This provides more accurate detection of duplicate
+ * application bundles that lead to undefined launch behavior.
+ */
+static uint64_t VersionForBundle(NSBundle *bundle) {
+    NSString *versionString = [bundle objectForInfoDictionaryKey:@"CFBundleVersion"];
+    return _LSGetVersionFromString((__bridge CFStringRef)versionString);
+}
+
+/**
+ * Returns a string representation of the specified Launch Services version number.
+ */
+static NSString *StringForVersion(uint64_t version) {
+    uint64_t major = version / 1000000;
+    version -= major * 1000000;
+
+    uint64_t minor = version / 1000;
+    version -= minor * 1000;
+
+    uint64_t patch = version;
+
+    if (patch > 0) {
+        return [NSString stringWithFormat:@"%llu.%llu.%llu", major, minor, patch];
+    } else if (minor > 0) {
+        return [NSString stringWithFormat:@"%llu.%llu", major, minor];
+    } else {
+        return [NSString stringWithFormat:@"%llu", major];
+    }
+}
+
+// TODO: Searching by URL may return different applications
+static void PrintAllBundles(NSBundle *resultBundle, NSArray *bundles) {
+    uint64_t resultVersion = VersionForBundle(resultBundle);
+    NSString *resultVersionString = StringForVersion(resultVersion);
+
+    bundles = [bundles sortedArrayUsingComparator:^NSComparisonResult(NSBundle *bundle1, NSBundle *bundle2) {
+        uint64_t version1 = VersionForBundle(bundle1);
+        uint64_t version2 = VersionForBundle(bundle2);
+
+        if (version2 > version1) {
+            return NSOrderedDescending;
+        } else if (version2 < version1) {
+            return NSOrderedAscending;
+        } else {
+            return [[bundle1 bundlePath] compare:[bundle2 bundlePath]];
+        }
+    }];
+
+    int versionLength = 0;
+    BOOL hasMultipleBundles = NO;
+
+    for (NSBundle *bundle in bundles) {
+        uint64_t version = VersionForBundle(bundle);
+        versionLength = MAX((int)[StringForVersion(version) length], versionLength);
+        if (version == resultVersion && [[bundle bundlePath] isEqualToString:[resultBundle bundlePath]] == NO) {
+            hasMultipleBundles = YES;
+        }
+    }
+
+    printf("*%-*s %s\n", versionLength + 1, [[resultVersionString stringByAppendingString:@":"] UTF8String], [[resultBundle bundlePath] fileSystemRepresentation]);
+
+    for (NSBundle *bundle in bundles) {
+        if ([[bundle bundlePath] isEqualToString:[resultBundle bundlePath]]) {
+            continue;
+        }
+
+        NSString *version = StringForVersion(VersionForBundle(bundle));
+        printf(" %-*s %s\n", versionLength + 1, [[version stringByAppendingString:@":"] UTF8String], [[bundle bundlePath] fileSystemRepresentation]);
+    }
+
+    if (hasMultipleBundles) {
+        printf("\nWarning: Multiple bundles found with version %s.\n", [resultVersionString UTF8String]);
+        printf("The result of Launch Services queries for this application will be undefined.\n\n");
+    }
+}
+
+static void SearchForBundleIdentifier(NSString *bundleIdentifier, BOOL findAll) {
+    NSBundle *resultBundle = BundleForBundleIdentifier(bundleIdentifier);
+    if (resultBundle == nil) {
+        fprintf(stderr, "No application found for \"%s\"\n", [bundleIdentifier UTF8String]);
+        exit(1);
+    }
+
+    if (findAll) {
+        NSArray *bundles = BundlesForBundleIdentifier(bundleIdentifier);
+        if (bundles == nil) {
+            fprintf(stderr, "Unable to query matching applications from Launch Services.\n");
+            exit(1);
+        }
+
+        PrintAllBundles(resultBundle, bundles);
+    } else {
+        printf("%s\n", [[resultBundle bundlePath] UTF8String]);
+    }
+}
+
+static void SearchForURL(NSURL *url, BOOL findAll) {
+    NSBundle *resultBundle = BundleForURL(url);
+    if (resultBundle == nil) {
+        fprintf(stderr, "No application found for \"%s\"\n", [[url absoluteString] UTF8String]);
+        exit(1);
+    }
+
+    if (findAll) {
+        NSArray *bundles = BundlesForURL(url);
+        if (bundles == nil) {
+            fprintf(stderr, "Unable to query matching applications from Launch Services.\n");
+            exit(1);
+        }
+
+        PrintAllBundles(resultBundle, bundles);
+    } else {
+        printf("%s\n", [[resultBundle bundlePath] UTF8String]);
+    }
+}
+
+static void print_usage() {
+    printf(
+    "Usage: lsquery <options>\n"
+    "\n"
+    "Queries the Launch Services database for matching application bundles.\n"
+    "\n"
+    "Options:\n"
+    "  -h, --help                 Show this help message and exit\n"
+    "  -b, --bundleid <bundleid>  Search for the given bundle identifier\n"
+    "  -u, --url <url>            Search for handlers of the given URL\n"
+    "      --find-all             Show all matching bundles\n"
+    "      --version              Show the version and exit \n");
+}
+
+int main(int argc, char *argv[]) {
     @autoreleasepool {
-        NSArray *arguments = [[NSProcessInfo processInfo] arguments];
-        if ([arguments count] < 2) {
+        NSString *bundleIdentifier = nil;
+        NSURL *url = nil;
+        BOOL findAll = NO;
+        int optchar;
+
+        static struct option longopts[] = {
+            { "help",            no_argument,        NULL,              'h' },
+            { "bundleid",        required_argument,  NULL,              'b' },
+            { "url",             required_argument,  NULL,              'u' },
+            { "find-all",        no_argument,        NULL,              'a' },
+            { NULL,              0,                  NULL,               0  }
+        };
+
+        while ((optchar = getopt_long(argc, argv, "hb:u:", longopts, NULL)) != -1) {
+            switch (optchar) {
+                case 'h':
+                    print_usage();
+                    return 0;
+                case 'b':
+                    bundleIdentifier = @(optarg);
+                    break;
+                case 'u':
+                    url = [NSURL URLWithString:@(optarg)];
+                    if (url == nil) {
+                        fprintf(stderr, "\"%s\" is not a valid URL\n", optarg);
+                        exit(1);
+                    }
+                    break;
+                case 'a':
+                    findAll = YES;
+                    break;
+                case '?':
+                    return 1;
+                default:
+                    fprintf(stderr, "unhandled option -%c\n", optchar);
+                    return 1;
+            }
+        }
+        argc -= optind;
+        argv += optind;
+
+        if (bundleIdentifier != nil) {
+            SearchForBundleIdentifier(bundleIdentifier, findAll);
+        } else if (url != nil) {
+            SearchForURL(url, findAll);
+        } else {
             print_usage();
             return 1;
         }
-
-        NSString *bundleIdentifier = arguments[1];
-
-        CFURLRef urlRef = NULL;
-        LSFindApplicationForInfo(0, (__bridge CFStringRef)bundleIdentifier, NULL, NULL, &urlRef);
-        if (urlRef == NULL) {
-            printf("No application found for \"%s\"\n", [bundleIdentifier UTF8String]);
-            return 1;
-        }
-
-        NSURL *bundleURL = CFBridgingRelease(urlRef);
-        NSBundle *resultBundle = [NSBundle bundleWithURL:bundleURL];
-        NSString *resultVersion = [resultBundle ms_bundleVersion];
-
-        NSArray *bundles = BundlesForBundleIdentifier(bundleIdentifier);
-        if (bundles == nil) {
-            printf("Unable to query matching applications from Launch Services.\n");
-            return 1;
-        }
-
-        bundles = [bundles sortedArrayUsingComparator:^NSComparisonResult(NSBundle *bundle1, NSBundle *bundle2) {
-            NSString *version1 = [bundle1 ms_bundleVersion];
-            NSString *version2 = [bundle2 ms_bundleVersion];
-            NSComparisonResult result = [version2 compare:version1 options:NSNumericSearch];
-            if (result == NSOrderedSame) {
-                result = [[bundle1 bundlePath] compare:[bundle2 bundlePath]];
-            }
-            return result;
-        }];
-
-        int versionLength = 0;
-        BOOL hasMultipleBundles = NO;
-
-        for (NSBundle *bundle in bundles) {
-            NSString *version = [bundle ms_bundleVersion];
-            versionLength = MAX((int)[version length], versionLength);
-            if ([version isEqualToString:resultVersion] && [[bundle bundlePath] isEqualToString:[resultBundle bundlePath]] == NO) {
-                hasMultipleBundles = YES;
-            }
-        }
-
-        printf("*%-*s %s\n", versionLength, [[resultVersion stringByAppendingString:@":"] UTF8String], [[resultBundle bundlePath] fileSystemRepresentation]);
-
-        for (NSBundle *bundle in bundles) {
-            if ([[bundle bundlePath] isEqualToString:[resultBundle bundlePath]]) {
-                continue;
-            }
-
-            NSString *version = [bundle ms_bundleVersion];
-            printf(" %-*s %s\n", versionLength + 1, [[version stringByAppendingString:@":"] UTF8String], [[bundle bundlePath] fileSystemRepresentation]);
-        }
-
-        if (hasMultipleBundles) {
-            printf("\nWarning: Multiple bundles found with version %s.\n", [resultVersion UTF8String]);
-            printf("The result of Launch Services queries for %s will be undefined.\n\n", [bundleIdentifier UTF8String]);
-        }
     }
+
     return 0;
 }
